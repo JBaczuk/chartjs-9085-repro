d3d0a64ac92d339d4818b9345ef890abad6e62f2
Jordan Baczuk <jordan.baczuk@qnergy.com>
2022-01-25 08:45:39 -0700

    initial repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
+import 'chartjs-adapter-luxon';
 import React from 'react';
-import logo from './logo.svg';
+import { Line } from 'react-chartjs-2';
 import './App.css';
 
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [{
+      label: 'My First dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+  }]
+}
+
 function App() {
+  const options = {
+    scales: {
+      y: {
+        min: 0,
+      },
+      x: {
+        type: 'time',
+        time: {
+          // Luxon Format Strings https://github.com/moment/luxon/blob/master/docs/formatting.md#table-of-tokens
+          // ChartJS Display Formats https://www.chartjs.org/docs/latest/axes/cartesian/time.html#display-formats
+          displayFormats: {
+            hour: 'h:mm a',
+            day: 'M/d ha',
+            weeks: 'M/d/yyyy',
+          },
+          minUnit: 'minute',
+          tooltipFormat: 'M/d/yyyy h:mm a',
+        },
+        ticks: {
+          source: 'data',
+          autoSkip: true,
+          autoSkipPadding: 15,
+
+          minRotation: 45,
+        },
+      },
+    },
+    maintainAspectRatio: false,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Line data={data} options={options} />
   );
 }
 
